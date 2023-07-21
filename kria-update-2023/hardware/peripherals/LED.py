798d96b837efdb683525e0a3c2259b959e4bd08f
Ryan Barry <rpb3646@rit.edu>
2023-07-21 19:19:55 -0400

    Added color switcher

--- kria-update-2023/hardware/peripherals/LED.py
+++ kria-update-2023/hardware/peripherals/LED.py
@@ -19,6 +19,24 @@ class LED:
         if color is not None:
             self.rgb = True
             self.set_color(color)
+        self.__switcher = {
+            "red": (255, 0, 0),
+            "green": (0, 255, 0),
+            "blue": (0, 0, 255),
+            "white": (255, 255, 255),
+            "black": (0, 0, 0),
+            "off": (0, 0, 0),
+            "yellow": (255, 255, 0),
+            "magenta": (255, 0, 255),
+            "cyan": (0, 255, 255),
+            "orange": (255, 128, 0),
+            "pink": (255, 192, 203),
+            "purple": (128, 0, 128),
+            "lavender": (230, 230, 250),
+            "turquoise": (64, 224, 208),
+            "gold": (255, 215, 0),
+            "silver": (192, 192, 192)
+        }
 
     def on(self):
         self.status = ON
@@ -26,6 +44,9 @@ class LED:
     def off(self):
         self.status = OFF
 
+    def get_color(self, color):
+        return self.__switcher[color]
+        
     def set_color(self, color):
         if self.rgb:
             self.status = color
