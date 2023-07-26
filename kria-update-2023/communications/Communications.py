edd6a248de927e33939bef484a58c5ae2062df48
Ryan Barry <rpb3646@rit.edu>
2023-07-25 21:34:21 -0400

    Status comm update

--- kria-update-2023/communications/Communications.py
+++ kria-update-2023/communications/Communications.py
@@ -7,13 +7,38 @@ from std_msgs.msg import Int32
 
 class Communications(Node):
     def __init__(self):
-        super().__init__("communication_antenna")
+        super().__init__("communications")
 
+        # Initialize class variables
         self.waypoint_status = WAYPOINT_INACTIVE
         self.comm_link_status = NOT_CONNECTED
+        self.operating_mode = DRIVER_CONTROL_MODE
+        self.temperature = 0
+        self.battery_voltage = 0
+        self.battery_current = 0
 
+        # Create publishers
+        self.operating_mode_pub = self.create_publisher(Int32, 'operating_mode_topic', 10)
+
+        # Create subscribers
         self.waypoint_status_subscriber = self.create_subscription(Int32, "waypoint_status", self.waypoint_status_callback, 10)
+        self.temperature_subscriber = self.create_subscription(Int32, "temperature", self.temperature_callback, 10)
+        self.battery_voltage_subscriber = self.create_subscription(Int32, "battery_voltage", self.battery_voltage_callback, 10)
+        self.battery_current_subscriber = self.create_subscription(Int32, "battery_current", self.battery_current_callback, 10)
 
 
     def waypoint_status_callback(self, msg):
         self.waypoint_status = msg
+
+    def temperature_callback(self, msg):
+        self.temperature = msg
+
+    def battery_voltage_callback(self, msg):
+        self.battery_voltage = msg
+
+    def battery_current_callback(self, msg):
+        self.battery_current = msg
+
+    def update_operating_mode(self, mode=DRIVER_CONTROL_MODE):
+        self.operating_mode = mode
+        self.operating_mode_pub.publish(self.operating_mode)
