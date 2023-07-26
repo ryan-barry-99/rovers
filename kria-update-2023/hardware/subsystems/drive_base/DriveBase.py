66ec03d9ddb23199b1df0bd3f935b56c17212568
Ryan Barry <rpb3646@rit.edu>
2023-07-25 21:29:50 -0400

    Create initial motor class

---
+++ kria-update-2023/hardware/subsystems/drive_base/DriveBase.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.append('../peripherals/')
+from peripherals.Motor import Motor
+
+class DriveBase:
+    def __init__(self):
+        self.fLeft = Motor()
+        self.mLeft = Motor()
+        self.bLeft = Motor()
+
+        self.fRight = Motor()
+        self.mRight = Motor()
+        self.bRight = Motor()
