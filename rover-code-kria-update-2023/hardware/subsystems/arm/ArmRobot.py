b77439e57cb42333c151e3081967583f94296cd1
Ryan Barry <rpb3646@rit.edu>
2023-08-12 15:15:29 -0400

    Update Arm Kinematics

--- rover-code-kria-update-2023/hardware/subsystems/arm/ArmRobot.py
+++ rover-code-kria-update-2023/hardware/subsystems/arm/ArmRobot.py
@@ -1,3 +1,14 @@
+"""
+File: ArmRobot.py
+
+Description:This module defines the ArmRobot class, which represents an arm robot with multiple joints. 
+It provides functionality to add links to the arm, move individual joints, update the Denavit-Hartenberg 
+(DH) table, and perform forward and inverse kinematics calculations.
+
+
+Author: Ryan Barry
+Date Created: August 11, 2023
+"""
 from ArmConstants import PRISMATIC, REVOLUTE
 
 class ArmRobot(ArmRobotKinematics):
@@ -5,7 +16,7 @@ class ArmRobot(ArmRobotKinematics):
         super().__init__()
         # Additional initialization code specific to the arm robot
 
-    def inverse_kinematics(self, desired_position, desired_orientation):
+    def inverse_kinematics(self, target_position, target_orientation):
         # Override the generic inverse kinematics method
         # Implement the specific inverse kinematics calculations for the arm robot
         pass
--- rover-code-kria-update-2023/hardware/subsystems/arm/ArmRobotKinematics.py
+++ rover-code-kria-update-2023/hardware/subsystems/arm/ArmRobotKinematics.py
@@ -11,6 +11,7 @@ Date created: June, 26 2023
 """
 
 import numpy as np  # Importing the NumPy library for mathematical operations
+from math import atan2, cos, sin, acos
 from ArmConstants import PRISMATIC, REVOLUTE
 
 
@@ -22,6 +23,7 @@ class ArmRobotKinematics:
         self.__a = []  # Private list to store a parameters
         self.__alpha = []  # Private list to store alpha parameters
         self.dhTable = None  # Initialize DH table as None
+        
 
     # Add a link to the arm
     # Parameters:
@@ -73,7 +75,8 @@ class ArmRobotKinematics:
         )  # Create the DH table using NumPy
 
     def forward_kinematics(self):
-        T = np.identity(4)  # Initialize the transformation matrix as an identity matrix
+        self.__A = [] # Initialize A matrices as empty list
+        self.__T = np.identity(4)  # Initialize the transformation matrix as an identity matrix
 
         for i in range(self.num_joints):
             theta = self.__theta[i]  # Get the joint angle
@@ -87,13 +90,15 @@ class ArmRobotKinematics:
                 ca = np.cos(alpha)  # Compute the cosine of alpha
                 sa = np.sin(alpha)  # Compute the sine of alpha
 
-                A = np.array(
-                    [
-                        [ct, -st * ca, st * sa, a * ct],  # Create the transformation matrix A
-                        [st, ct * ca, -ct * sa, a * st],
-                        [0, sa, ca, d],
-                        [0, 0, 0, 1],
-                    ]
+                self.__A.append(
+                    np.array(
+                        [
+                            [ct, -st * ca, st * sa, a * ct],  # Create the transformation matrix A
+                            [st, ct * ca, -ct * sa, a * st],
+                            [0, sa, ca, d],
+                            [0, 0, 0, 1],
+                        ]
+                    )
                 )
 
             elif self.__joint_type[i] == PRISMATIC:  # Check if the joint type is prismatic
@@ -102,13 +107,15 @@ class ArmRobotKinematics:
                 ca = np.cos(alpha)  # Compute the cosine of alpha
                 sa = np.sin(alpha)  # Compute the sine of alpha
 
-                A = np.array(
-                    [
-                        [ct, -st * ca, st * sa, ct * d],  # Create the transformation matrix A
-                        [st, ct * ca, -ct * sa, st * d],
-                        [0, sa, ca, a],
-                        [0, 0, 0, 1],
-                    ]
+                self.__A.append(
+                    np.array(
+                        [
+                            [ct, -st * ca, st * sa, ct * d],  # Create the transformation matrix A
+                            [st, ct * ca, -ct * sa, st * d],
+                            [0, sa, ca, a],
+                            [0, 0, 0, 1],
+                        ]
+                    )
                 )
             else:
                 print(
@@ -116,11 +123,41 @@ class ArmRobotKinematics:
                 )  # Print an error message for invalid joint type
                 return None
 
-            T = np.dot(T, A)  # Multiply the transformation matrix T by A
+            self.__T = np.dot(self.__T, self.__A[i])  # Multiply the transformation matrix T by A
 
-        return T  # Return the final transformation matrix
+        return self.__T  # Return the final transformation matrix
     
     
-    def inverse_kinematics(self, desired_position, desired_orientation):
-        # Generic implementation of inverse kinematics
+    def inverse_kinematics(self, target_position, target_orientation):
         pass
+            
+    
+    # def inverse_kinematics(self, target_position, target_orientation):
+    #     joint_angles = [0.0]*self.num_joints
+        
+    #     # Perform the inverse kinematics calculation
+    #     for i in range(self.num_joints):
+    #         theta, d, a, alpha = self.dh_table[i]
+
+    #         if i == 0:
+    #             joint_angles[i] = atan2(target_position[1], target_position[0])
+    #         else:
+    #             link_position = np.array([0.0, 0.0, 0.0])
+    #             for j in range(i):
+    #                 theta_j, d_j, a_j, alpha_j = self.dh_table[j]
+    #                 link_position[0] += a_j * cos(joint_angles[j])
+    #                 link_position[1] += a_j * sin(joint_angles[j])
+    #             link_position[0] += a * cos(joint_angles[i])
+    #             link_position[1] += a * sin(joint_angles[i])
+
+    #             target_distance = np.linalg.norm(target_position[:2] - link_position[:2])
+    #             target_angle = atan2(target_position[1] - link_position[1], target_position[0] - link_position[0])
+    #             joint_angles[i] = target_angle - acos(target_distance / a)
+
+    #     # Calculate the joint angles for target orientation
+    #     # Modify this part based on your specific orientation representation and calculation method
+    #     # Here, we assume the target_orientation is represented as Euler angles (roll, pitch, yaw)
+    #     target_roll, target_pitch, target_yaw = target_orientation
+    #     # Add your calculation method here to determine the joint angles for orientation
+
+    #     return joint_angles
