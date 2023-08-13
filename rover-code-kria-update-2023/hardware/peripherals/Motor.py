e8d5a801af7d7b1ee44cc2370f406c3ca5d8e425
Ryan Barry <rpb3646@rit.edu>
2023-08-13 17:01:23 -0400

    Add GPIO support

--- rover-code-kria-update-2023/hardware/peripherals/Motor.py
+++ rover-code-kria-update-2023/hardware/peripherals/Motor.py
@@ -1,3 +1,13 @@
+from pynq.lib.arduino import Arduino_IO
+
 class Motor:
     def __init__(self):
-        pass
+        self.gpio_pin = None
+
+    def select_gpio_pin(self, pin_number):
+        self.gpio_pin = Arduino_IO(pin_number, 'out')
+
+    def update_gpio(self, value):
+        if self.gpio_pin is None:
+            raise ValueError("No GPIO pin selected. Call select_gpio_pin() first.")
+        self.gpio_pin.write(value)
--- rover-code-kria-update-2023/hardware/subsystems/drive_base/DriveBase.py
+++ rover-code-kria-update-2023/hardware/subsystems/drive_base/DriveBase.py
@@ -1,15 +1,24 @@
 import sys
+sys.path.append("../..")
+from RoverPinout import *
+from DriveWheel import DriveWheel
+from DifferentialDrive import DifferentialDrive
 
-sys.path.append("../peripherals/")
-from peripherals.Motor import Motor
 
-
-class DriveBase:
+class DriveBase(DifferentialDrive):
     def __init__(self):
-        self.fLeft = Motor()
-        self.mLeft = Motor()
-        self.bLeft = Motor()
+        self.fLeft = DriveWheel(name="frontLeft", gpio_pin=FRONT_LEFT_PIN)
+        self.mLeft = DriveWheel(name="middleLeft", gpio_pin=MIDDLE_LEFT_PIN)
+        self.bLeft = DriveWheel(name="backLeft", gpio_pin=BACK_LEFT_PIN)
 
-        self.fRight = Motor()
-        self.mRight = Motor()
-        self.bRight = Motor()
+        self.fRight = DriveWheel(name="frontRight", gpio_pin=FRONT_RIGHT_PIN)
+        self.mRight = DriveWheel(name="middleRight", gpio_pin=MIDDLE_RIGHT_PIN)
+        self.bRight = DriveWheel(name="backRight", gpio_pin=BACK_RIGHT_PIN)
+        
+        self.left_velo = 0.0
+        self.right_velo = 0.0
+        self.linear_velocity = 0.0
+        self.angular_velocity = 0.0
+        
+    def get_velo(self):
+        self.linear_velocity, self.angular_velocity = self.forward_kinematics(self.left_velo, self.right_velo)
