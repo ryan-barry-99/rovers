d66c3f567913c314fef8706d8ca5d17dd11a2b58
Ryan Barry <rpb3646@rit.edu>
2023-08-26 22:56:13 -0400

    Restructure Main

--- rover-code-kria-update-2023/rover_main.py
+++ rover-code-kria-update-2023/rover_main.py
@@ -4,47 +4,15 @@ File: rover_main.py
 
 Description: This script serves as the main control module for a rover system. 
 It utilizes the Robot Operating System (ROS) client library (rclpy) to interface 
-with the rover's hardware components. The program defines a MissionControl class 
-responsible for managing various missions that the rover can execute. Each mission 
-is represented by a separate class: AutonomousNavigationMission, 
-EquipmentServicingMission, ExtremeRetrievalDeliveryMission, and ScienceMission.
+with the rover's hardware components. The program instantiates the MissionControl class 
+responsible for managing various missions that the rover can execute.
 
 Author: Ryan Barry
-Date Created: July 16, 2023
+Date Created: August 26, 2023
 """
 
 import rclpy
-from communications.XBox_Controller import XboxController
-from hardware.Rover import Rover
-from hardware.RoverConstants import *
-from missions.AutonomousNavigation import AutonomousNavigationMission
-from missions.EquipmentServicing import EquipmentServicingMission
-from missions.ExtremeRetrievalDelivery import ExtremeRetrievalDeliveryMission
-from missions.ScienceMission import ScienceMission
-
-
-class MissionControl:
-    def __init__(self):
-        self.rover = Rover()
-        self.autonomous_navigation = AutonomousNavigationMission(self.rover)
-        self.equipment_servicing = EquipmentServicingMission(self.rover)
-        self.extreme_retrieval_delivery = ExtremeRetrievalDeliveryMission(self.rover)
-        self.science_mission = ScienceMission(self.rover)
-        self.controller = XboxController()
-
-    def exec(self):
-        self.controller.publish_controller_state()
-
-        mission = self.rover.get_mission()
-
-        if mission == AUTONOMOUS:
-            self.autonomous_navigation.run()
-        if mission == EQUIPMENT_SERVICING:
-            self.equipment_servicing.run()
-        if mission == EXTREME_RETRIEVAL_DELIVERY:
-            self.extreme_retrieval_delivery.run()
-        if mission == SCIENCE:
-            self.science_mission.run()
+from missions.MissionControl import MissionControl
 
 
 def main(args=None):
@@ -59,4 +27,7 @@ def main(args=None):
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except: 
+        rclpy.shutdown()
