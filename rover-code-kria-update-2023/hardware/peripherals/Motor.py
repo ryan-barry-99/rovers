dab7bbced21157055ed20df4d8463ed3153b6309
Ryan Barry <rpb3646@rit.edu>
2023-09-12 22:44:53 -0400

    Update Motor Class

--- rover-code-kria-update-2023/hardware/peripherals/Motor.py
+++ rover-code-kria-update-2023/hardware/peripherals/Motor.py
@@ -1,21 +1,23 @@
 from pynq.lib.rpi import GPIO
+import time
 
 
 class Motor:
     def __init__(self, pwm_pin=None):
-        self.pwm_pin = pwm_pin
+        self._pwm_pin = pwm_pin
+        self.duty_cycle = 0.0
 
     def select_pwm_pin(self, pin_number):
-        self.pwm_pin = Arduino_IO(pin_number, "out")
+        self._pwm_pin = Arduino_IO(pin_number, "out")
 
     def update_pwm(self):
         current_time = time.monotonic()
         if current_time >= self.next_toggle_time:
-            if GPIO.input(self.pwm_pin) == GPIO.HIGH:
+            if GPIO.input(self._pwm_pin) == GPIO.HIGH:
                 off_time = 1 / self.frequency * (1 - (self.duty_cycle / 100))
                 self.next_toggle_time = current_time + off_time
-                GPIO.output(self.pwm_pin, GPIO.LOW)
+                GPIO.output(self._pwm_pin, GPIO.LOW)
             else:
                 on_time = 1 / self.frequency * (self.duty_cycle / 100)
                 self.next_toggle_time = current_time + on_time
-                GPIO.output(self.pwm_pin, GPIO.HIGH)
+                GPIO.output(self._pwm_pin, GPIO.HIGH)
