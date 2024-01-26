a67018cbe18f6f93b62e98f2f094dfde0a6e4c5d
Ryan Barry <94549600+ryan-barry-99@users.noreply.github.com>
2024-01-25 20:27:51 -0500

    Update drive_base.cpp

--- rover-code/embedded/main_body_board/src/drive_base/drive_base.cpp
+++ rover-code/embedded/main_body_board/src/drive_base/drive_base.cpp
@@ -10,29 +10,7 @@ DriveBase::DriveBase()
         Wheel(PWM_PIN_4, ENC_A_PIN_4, ENC_B_PIN_4),
         Wheel(PWM_PIN_5, ENC_A_PIN_5, ENC_B_PIN_5)
     }
-{
-    // Set up drive base pins
-    pinMode(PWM_PIN_0, OUTPUT);
-    pinMode(PWM_PIN_1, OUTPUT);
-    pinMode(PWM_PIN_2, OUTPUT);
-    pinMode(PWM_PIN_3, OUTPUT);
-    pinMode(PWM_PIN_4, OUTPUT);
-    pinMode(PWM_PIN_5, OUTPUT);
-
-    pinMode(ENC_A_PIN_0, INPUT);
-    pinMode(ENC_A_PIN_1, INPUT);
-    pinMode(ENC_A_PIN_2, INPUT);
-    pinMode(ENC_A_PIN_3, INPUT);
-    pinMode(ENC_A_PIN_4, INPUT);
-    pinMode(ENC_A_PIN_5, INPUT);
-
-    pinMode(ENC_B_PIN_0, INPUT);
-    pinMode(ENC_B_PIN_1, INPUT);
-    pinMode(ENC_B_PIN_2, INPUT);
-    pinMode(ENC_B_PIN_3, INPUT);
-    pinMode(ENC_B_PIN_4, INPUT);
-    pinMode(ENC_B_PIN_5, INPUT);
-}
+{}
 
 void DriveBase::updateVelocity() {
     for (int i = 0; i < 6; i++) {
