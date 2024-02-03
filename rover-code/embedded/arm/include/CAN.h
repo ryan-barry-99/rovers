ae0abe73dc9f701270a0641dc17884efedcafc81
Ryan Barry <rpb3646@rit.edu>
2024-02-03 12:30:29 -0500

    Add CAN to each board

---
+++ rover-code/embedded/arm/include/CAN.h
@@ -0,0 +1,30 @@
+#ifndef CAN_H
+#define CAN_H
+#include <FlexCAN_T4.h>
+#include <unordered_map>
+#include "pinout.h"
+
+using ObjectDictionary = std::unordered_map<uint32_t, CAN_message_t>;
+
+class CAN
+{
+    public:    
+        enum CAN_MB{
+            JETSON = FLEXCAN_MAILBOX::MB0, 
+            MAIN_BODY = FLEXCAN_MAILBOX::MB1, 
+            SCIENCE_BOARD = FLEXCAN_MAILBOX::MB2,
+            ARM_BOARD = FLEXCAN_MAILBOX::MB3
+            };
+
+
+        CAN(CAN_MB mailBox);
+
+        void SendMessage( CAN_MB mailBox, uint32_t id, uint8_t message[8]);
+        auto GetMessage(uint32_t id);
+
+    private:
+        FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_16> m_CAN;
+        static ObjectDictionary m_objectDict;
+        static void CANSniff(const CAN_message_t &msg);
+};
+#endif
---
+++ rover-code/embedded/arm/src/CAN.cpp
@@ -0,0 +1,76 @@
+#include "../include/CAN.h"
+#include <FlexCAN_T4.h>
+
+
+CAN::CAN(CAN_MB mailBox)
+{
+  m_CAN.enableMBInterrupt((FLEXCAN_MAILBOX) mailBox);
+
+  
+  m_CAN.setMB( (FLEXCAN_MAILBOX)CAN::MAIN_BODY,     TX, STD); // Set the mailbox to transmit
+  m_CAN.setMB( (FLEXCAN_MAILBOX)CAN::JETSON,        TX, STD); // Set the mailbox to transmit
+  m_CAN.setMB( (FLEXCAN_MAILBOX)CAN::SCIENCE_BOARD, TX, STD); // Set the mailbox to transmit
+  m_CAN.setMB( (FLEXCAN_MAILBOX)CAN::ARM_BOARD,     TX, STD); // Set the mailbox to transmit
+
+  switch(mailBox){    
+    case CAN::CAN_MB::MAIN_BODY:
+      m_CAN.setMB( (FLEXCAN_MAILBOX)CAN::MAIN_BODY, RX, STD); // Set the mailbox to receive
+      break;
+    
+    case CAN_MB::JETSON:
+      m_CAN.setMB( (FLEXCAN_MAILBOX)CAN::JETSON, RX, STD); // Set the mailbox to receive
+      break;
+
+    case CAN::CAN_MB::SCIENCE_BOARD:
+      m_CAN.setMB( (FLEXCAN_MAILBOX)CAN::SCIENCE_BOARD, RX, STD); // Set the mailbox to receive
+      break;
+    
+    case CAN::CAN_MB::ARM_BOARD:
+      m_CAN.setMB( (FLEXCAN_MAILBOX)CAN::ARM_BOARD, RX, STD); // Set the mailbox to receive
+      break;
+  }
+
+  // Start the CAN bus
+  m_CAN.begin(); // <- This is needed
+
+  // Set the baud rate to 115200
+  m_CAN.setBaudRate(115200); 
+
+  // Set the interrupt to call the canSniff function
+  m_CAN.onReceive((FLEXCAN_MAILBOX)mailBox, &CAN::CANSniff);
+  
+}
+
+void CAN::CANSniff(const CAN_message_t &msg)
+{
+  m_objectDict[msg.id] = msg;
+}
+
+// Send a message to the CAN bus
+void CAN::SendMessage( CAN_MB mailBox, uint32_t id, uint8_t message[8])
+{
+  // Create a message
+  CAN_message_t msg;
+
+  // Set the message ID to 0x123
+  msg.id = id;
+  
+  // Set the message length to 8
+  msg.len = 8;
+
+  // Set the message buffer to the m_message buffer
+  for(int i = 0; i < 8; i++)
+  {
+    msg.buf[i] = message[i];
+  }
+
+  // Send the message
+  m_CAN.write( (FLEXCAN_MAILBOX)mailBox, msg);
+
+}
+
+// Retrieve a message from the object dictionary
+auto CAN::GetMessage(uint32_t id)
+{
+  return m_objectDict[id];
+}
