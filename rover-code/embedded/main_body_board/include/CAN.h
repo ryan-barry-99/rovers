aad8e81b5871a8f0aa455e71303314015dfb1c44
Ryan Barry <rpb3646@rit.edu>
2024-02-03 12:27:01 -0500

    Object Dctionary Manipulation Static

--- rover-code/embedded/main_body_board/include/CAN.h
+++ rover-code/embedded/main_body_board/include/CAN.h
@@ -9,30 +9,22 @@ using ObjectDictionary = std::unordered_map<uint32_t, CAN_message_t>;
 class CAN
 {
     public:    
-    
-    enum CAN_MB{
-        JETSON = FLEXCAN_MAILBOX::MB0, 
-        MAIN_BODY = FLEXCAN_MAILBOX::MB1, 
-        SCIENCE_BOARD = FLEXCAN_MAILBOX::MB2,
-        ARM_BOARD = FLEXCAN_MAILBOX::MB3
-        };
+        enum CAN_MB{
+            JETSON = FLEXCAN_MAILBOX::MB0, 
+            MAIN_BODY = FLEXCAN_MAILBOX::MB1, 
+            SCIENCE_BOARD = FLEXCAN_MAILBOX::MB2,
+            ARM_BOARD = FLEXCAN_MAILBOX::MB3
+            };
 
 
-    CAN(CAN::CAN_MB mailBox);
+        CAN(CAN_MB mailBox);
 
-    void SendMessage( CAN_MB mailBox, uint32_t id, uint8_t message[8]);
+        void SendMessage( CAN_MB mailBox, uint32_t id, uint8_t message[8]);
+        auto GetMessage(uint32_t id);
 
-    // void canSniff(const CAN_message_t &msg);
-    auto GetMessage();
-
-    //void test(const CAN_message_t &msg);
-
-    // static void staticCanSniff(const CAN_message_t &msg);
-
-    ObjectDictionary m_objectDict;
-    
     private:
-    
-    FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_16> m_CAN;
+        FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_16> m_CAN;
+        static ObjectDictionary m_objectDict;
+        static void CANSniff(const CAN_message_t &msg);
 };
 #endif
--- rover-code/embedded/main_body_board/src/CAN.cpp
+++ rover-code/embedded/main_body_board/src/CAN.cpp
@@ -1,28 +1,9 @@
 #include "../include/CAN.h"
 #include <FlexCAN_T4.h>
 
-//#include <functional>
 
-uint8_t m_CANMessage[8];
-
-void CANSniff(const CAN_message_t &msg)
+CAN::CAN(CAN_MB mailBox)
 {
-  msg.id;
-
-  for(int i = 0; i < 8; i++)
-  {
-    m_CANMessage[i] = msg.buf[i];    
-  } 
-}
-
-CAN::CAN(CAN::CAN_MB mailBox)
-{
-  //std::bind<void>(test, this);
-
-
-
-  // m_CAN.setRX(CAN_pins::CAN_RX_PIN);
-  // m_CAN.setTX(CAN_pins::CAN_TX_PIN);
   m_CAN.enableMBInterrupt((FLEXCAN_MAILBOX) mailBox);
 
   
@@ -49,15 +30,20 @@ CAN::CAN(CAN::CAN_MB mailBox)
       break;
   }
 
-// Start the CAN bus
+  // Start the CAN bus
   m_CAN.begin(); // <- This is needed
 
   // Set the baud rate to 115200
   m_CAN.setBaudRate(115200); 
 
   // Set the interrupt to call the canSniff function
-  m_CAN.onReceive((FLEXCAN_MAILBOX)mailBox, CANSniff);
+  m_CAN.onReceive((FLEXCAN_MAILBOX)mailBox, &CAN::CANSniff);
   
+}
+
+void CAN::CANSniff(const CAN_message_t &msg)
+{
+  m_objectDict[msg.id] = msg;
 }
 
 // Send a message to the CAN bus
@@ -83,7 +69,8 @@ void CAN::SendMessage( CAN_MB mailBox, uint32_t id, uint8_t message[8])
 
 }
 
-auto CAN::GetMessage()
+// Retrieve a message from the object dictionary
+auto CAN::GetMessage(uint32_t id)
 {
-
+  return m_objectDict[id];
 }
